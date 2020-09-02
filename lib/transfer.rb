37e344e21e99db7761b28d5b45851221ef034bfe
Richard Kurwitz <kurwitz3@aol.com>
2020-09-02 00:36:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
            sender.balance = sender.balance - amount 
       @status = "complete"
     else 
-      //@status = "rejected"  
+      #@status = "rejected"  
      return  "Transaction rejected. Please check your account balance."
     end
  end 
