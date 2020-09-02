061cfb5187ee423a3e7a16ce6dd9e1853c5baa8e
Richard Kurwitz <kurwitz3@aol.com>
2020-09-02 00:08:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
       
         return   "Transaction rejected. Please check your account balance."
          @status = "rejected"
+         binding.pry 
           elsif  @status == "pending"
            receiver.deposit(amount )
            sender.balance = sender.balance - amount 
